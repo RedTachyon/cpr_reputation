88233f3f25f8814b1d36acc53b2313d4814d6a87
bengreenberg5 <bengreenberg5@gmail.com>
2021-04-03 12:49:42 -0400

    roll back environments

--- cpr_reputation/environments.py
+++ cpr_reputation/environments.py
@@ -1,10 +1,7 @@
-import numpy as np
-import random
 from typing import Dict
-
-from cpr_reputation.board import HarvestGame, SHOOT, regenerate_apples
-from gym.spaces import Box, Discrete
+import numpy as np
 from ray.rllib.env import MultiAgentEnv as RayMultiAgentEnv
+from cpr_reputation.board import HarvestGame
 
 
 class HarvestEnv(RayMultiAgentEnv):
@@ -13,16 +10,9 @@ class HarvestEnv(RayMultiAgentEnv):
         self.config = config
         self.time = 0
         self.game = HarvestGame(**kwargs)
-        self.original_board = np.copy(self.game.board)
 
-        self.observation_space = Box(
-            0, 1, (self.game.sight_dist * (self.game.sight_width * 2 + 1),)
-        )
-        self.action_space = Discrete(8)
-
-    def reset(self, *args, **kwargs) -> Dict[str, np.ndarray]:
+    def reset(self) -> Dict[str, np.ndarray]:
         self.game.reset()
-        self.original_board = np.copy(self.game.board)
         self.time = 0
         return {
             agent_id: self.game.get_agent_obs(agent_id)
@@ -31,9 +21,7 @@ class HarvestEnv(RayMultiAgentEnv):
 
     def step(self, actions: Dict[str, int]):
         rewards = {agent_id: 0.0 for agent_id in self.game.agents}
-        action_pairs = list(actions.items())
-        random.shuffle(action_pairs)
-        for (agent_id, action) in action_pairs:
+        for (agent_id, action) in actions.items():
             reward = self.game.process_action(agent_id, action)
             rewards[agent_id] += reward
 
@@ -42,18 +30,9 @@ class HarvestEnv(RayMultiAgentEnv):
             for agent_id, _ in self.game.agents.items()
         }
 
-        done = {agent_id: self.time > 1000 for agent_id, _ in
-                self.game.agents.items()}
-        # done["__all__"] = self.time > 1000  # Required for rllib (I think)
-
-        num_shots = sum(1 for key, action in actions.items() if action == SHOOT)
-        info = {"m_shots": num_shots}
-        self.game.board = regenerate_apples(self.game.board)
-        self.game.board = self.game.board * self.original_board
-
+        done = {agent_id: self.time > 1000 for agent_id, _ in self.game.agents.items()}
+        done["__all__"] = self.time > 1000  # Required for rllib (I think)
+        info = {}
         self.time += 1
 
         return obs, rewards, done, info
-
-    def render(self, *args, **kwargs):
-        return self.game.render(*args, **kwargs)
