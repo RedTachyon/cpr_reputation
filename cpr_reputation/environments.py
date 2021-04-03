fb1c3729263a06d19e1b1cf62d5f914a561c8cd5
bengreenberg5 <bengreenberg5@gmail.com>
2021-04-03 13:37:39 -0400

    regenerate apples before getting observations

--- cpr_reputation/environments.py
+++ cpr_reputation/environments.py
@@ -1,7 +1,10 @@
-from typing import Dict
 import numpy as np
+import random
+from typing import Dict
+
 from ray.rllib.env import MultiAgentEnv as RayMultiAgentEnv
-from cpr_reputation.board import HarvestGame
+
+from cpr_reputation.board import HarvestGame, regenerate_apples
 
 
 class HarvestEnv(RayMultiAgentEnv):
@@ -10,6 +13,7 @@ class HarvestEnv(RayMultiAgentEnv):
         self.config = config
         self.time = 0
         self.game = HarvestGame(**kwargs)
+        self.original_board = np.copy(self.game.board)
 
     def reset(self) -> Dict[str, np.ndarray]:
         self.game.reset()
@@ -20,19 +24,27 @@ class HarvestEnv(RayMultiAgentEnv):
         }
 
     def step(self, actions: Dict[str, int]):
+        # process actions and rewards
         rewards = {agent_id: 0.0 for agent_id in self.game.agents}
-        for (agent_id, action) in actions.items():
+        action_pairs = list(actions.items())
+        random.shuffle(action_pairs)
+        for (agent_id, action) in action_pairs:
             reward = self.game.process_action(agent_id, action)
             rewards[agent_id] += reward
 
+        # update environment
+        self.game.board = regenerate_apples(self.game.board)
+        self.game.board = self.game.board * self.original_board
+        self.time += 1
+
+        # get observations, done, info
         obs = {
             agent_id: self.game.get_agent_obs(agent_id)
             for agent_id, _ in self.game.agents.items()
         }
-
-        done = {agent_id: self.time > 1000 for agent_id, _ in self.game.agents.items()}
-        done["__all__"] = self.time > 1000  # Required for rllib (I think)
+        done = {}
+        done["__all__"] = self.time > 1000 or self.game.board.sum() == 0
         info = {}
-        self.time += 1
 
         return obs, rewards, done, info
+
