201a0981b4b3e767eef0d9b843287a6530bc8837
Ben Greenberg <bengreenberg5@gmail.com>
2021-06-24 20:20:47 -0400

    compute all agent observations at once

--- cpr_reputation/environments.py
+++ cpr_reputation/environments.py
@@ -27,20 +27,12 @@ class HarvestEnv(RayMultiAgentEnv):
     def reset(self) -> Dict[str, np.ndarray]:
         self.game.reset()
         self.original_board = np.copy(self.game.board)
-        return {
-            agent_id: self.game.get_agent_obs(agent_id)
-            for agent_id, _ in self.game.agents.items()
-        }
+        return self.game.get_agents_obs()
 
     def step(self, actions: Dict[str, int]):
-
         rewards = self.game.step(actions)
 
-        # get observations, done, info
-        obs = {
-            agent_id: self.game.get_agent_obs(agent_id)
-            for agent_id, _ in self.game.agents.items()
-        }
+        obs = self.game.get_agents_obs()
 
         isdone = self.game.time > 1000 or self.game.board.sum() == 0
         done = {agent_id: isdone for agent_id, _ in self.game.agents.items()}
